a61d758448852d9a89c63e05d5fce580c3f33131
Владимир <RusiSLin@gmail.com>
2024-10-05 10:47:40 +0300

    подправил некоторые недоработки

--- time.pas
+++ time.pas
@@ -1,4 +1,5 @@
 ﻿begin
   var year := readinteger('Введите год');
-  Writeln(if (year mod 4 = 0) and (not(year mod 100 = 0) or (year mod 400 = 0)) then 366 else 365);
+  if (year mod 4 = 0) and (not(year mod 100 = 0) or (year mod 400 = 0)) then Writeln(366)
+  else Writeln(365)
 end.
--- timer.pas
+++ timer.pas
@@ -1,4 +1,7 @@
 ﻿begin
-  var year := readinteger('Введите год');
-  Writeln((year mod 4 = 0) and (not(year mod 100 = 0) or (year mod 400 = 0)));
+  var hours :=  readinteger('введите количество часов');
+  var minutes :=  readinteger('введите количество минут');
+  var seconds :=  readinteger('введите количество секунд');
+  Assert((minutes < 60) and (seconds < 60) and (minutes >= 0) and (seconds >= 0) and (hours >= 0));
+  Writeln($'Общее количество секунд: {hours * 3600 + minutes * 60 + seconds}');
 end.
