af7a052447b3631c6c25c2386caaf102d3d3c2d6
Владимир <RusiSLin@gmail.com>
2024-10-05 10:34:15 +0300

    добавил time.pas

--- hello.pas
+++ hello.pas
@@ -1,5 +1,6 @@
 ﻿begin
   var time := readinteger('Введите текущий час');
+  assert((time>=0) and (time <= 23));
   case time of 
     4..10:Print('Доброе утро, мир!');
     11..16:Print('Добрый день, мир!');
