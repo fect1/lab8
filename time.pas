f09e977bc64e01e8049b24cbf212480d3ea87978
Владимир <RusiSLin@gmail.com>
2024-10-05 10:42:15 +0300

    изменил вывод time.pas, добавил timer.pas

--- time.pas
+++ time.pas
@@ -1,4 +1,4 @@
 ﻿begin
   var year := readinteger('Введите год');
-  Writeln((year mod 4 = 0) and (not(year mod 100 = 0) or (year mod 400 = 0)));
+  Writeln(if (year mod 4 = 0) and (not(year mod 100 = 0) or (year mod 400 = 0)) then 366 else 365);
 end.
---
+++ timer.pas
@@ -0,0 +1,4 @@
+﻿begin
+  var year := readinteger('Введите год');
+  Writeln((year mod 4 = 0) and (not(year mod 100 = 0) or (year mod 400 = 0)));
+end.
